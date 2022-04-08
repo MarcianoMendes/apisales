002868c03a78302e0b908c1b10d666b804318026
MarcianoMendes <nmendes94@hotmail.com>
2022-04-08 09:31:28 -0300

    first commit

---
+++ src/modules/products/routes/products.routes.ts
@@ -0,0 +1,38 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { RequestHandler, Router } from 'express';
+import ProductController from '../controllers/ProductController';
+
+const seguimentParams = {
+  id: Joi.string().uuid().required(),
+};
+
+const seguimentBody = {
+  name: Joi.string().required(),
+  price: Joi.number().precision(2).required(),
+  quantity: Joi.number().required(),
+};
+
+function validateParams(): RequestHandler {
+  return celebrate({ [Segments.PARAMS]: seguimentParams });
+}
+
+function validateBody(): RequestHandler {
+  return celebrate({ [Segments.BODY]: seguimentBody });
+}
+
+function validateParamsAndBody(): RequestHandler {
+  return celebrate({
+    [Segments.PARAMS]: seguimentParams,
+    [Segments.BODY]: seguimentBody,
+  });
+}
+
+const productsRouter = Router();
+const productController = new ProductController();
+productsRouter.get('/', productController.index);
+productsRouter.get('/:id', validateParams(), productController.show);
+productsRouter.post('/', validateBody(), productController.create);
+productsRouter.put('/:id', validateParamsAndBody(), productController.update);
+productsRouter.delete('/:id', validateParams(), productController.delete);
+
+export default productsRouter;
---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
+
+const routes = Router();
+routes.use('/products', productsRouter);
+export default routes;
